92a07a0976467a56a941882b8d5a1a35dbe915c8
Muaz Khalid <itzmajo786@gmail.com>
2024-11-21 16:04:41 +0500

    Fixing Issues

--- assets/Javascript/java.js
+++ assets/Javascript/java.js
@@ -30,3 +30,38 @@ function toggleSidebar() {
     document.body.style.overflow = "auto";
   }
 }
+
+
+function toggleFAQ(element) {
+  // Close all other FAQ answers
+  const allAnswers = document.querySelectorAll('.faq-answer');
+  const allIcons = document.querySelectorAll('.faq-question i');
+  
+  allAnswers.forEach(answer => {
+    if (answer !== element.nextElementSibling) {
+      answer.classList.add('hidden');
+    }
+  });
+  
+  allIcons.forEach(icon => {
+    if (icon !== element.querySelector('i')) {
+      icon.classList.remove('fa-minus');
+      icon.classList.add('fa-plus');
+    }
+  });
+
+  // Toggle the clicked FAQ
+  const answer = element.nextElementSibling;
+  const icon = element.querySelector('i');
+  
+  answer.classList.toggle('hidden');
+  
+  if (answer.classList.contains('hidden')) {
+    icon.classList.remove('fa-minus');
+    icon.classList.add('fa-plus');
+  } else {
+    icon.classList.remove('fa-plus');
+    icon.classList.add('fa-minus');
+  }
+}
+
