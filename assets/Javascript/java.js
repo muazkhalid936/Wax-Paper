39caf610b338d0c1f5e00e33686b5451ebd8c418
Muaz Khalid <itzmajo786@gmail.com>
2024-12-05 15:15:46 +0500

    Ok ho gya

--- assets/Javascript/java.js
+++ assets/Javascript/java.js
@@ -31,37 +31,36 @@ function toggleSidebar() {
   }
 }
 
-
 function toggleFAQ(element) {
   // Close all other FAQ answers
-  const allAnswers = document.querySelectorAll('.faq-answer');
-  const allIcons = document.querySelectorAll('.faq-question i');
-  
-  allAnswers.forEach(answer => {
+  const allAnswers = document.querySelectorAll(".faq-answer");
+  const allIcons = document.querySelectorAll(".faq-question i");
+
+  allAnswers.forEach((answer) => {
     if (answer !== element.nextElementSibling) {
-      answer.classList.add('hidden');
+      answer.classList.add("hidden");
     }
   });
-  
-  allIcons.forEach(icon => {
-    if (icon !== element.querySelector('i')) {
-      icon.classList.remove('fa-minus');
-      icon.classList.add('fa-plus');
+
+  allIcons.forEach((icon) => {
+    if (icon !== element.querySelector("i")) {
+      icon.classList.remove("fa-minus");
+      icon.classList.add("fa-plus");
     }
   });
 
   // Toggle the clicked FAQ
   const answer = element.nextElementSibling;
-  const icon = element.querySelector('i');
-  
-  answer.classList.toggle('hidden');
-  
-  if (answer.classList.contains('hidden')) {
-    icon.classList.remove('fa-minus');
-    icon.classList.add('fa-plus');
+  const icon = element.querySelector("i");
+
+  answer.classList.toggle("hidden");
+
+  if (answer.classList.contains("hidden")) {
+    icon.classList.remove("fa-minus");
+    icon.classList.add("fa-plus");
   } else {
-    icon.classList.remove('fa-plus');
-    icon.classList.add('fa-minus');
+    icon.classList.remove("fa-plus");
+    icon.classList.add("fa-minus");
   }
 }
 
@@ -82,4 +81,21 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     },
   }).mount();
+});
+
+let lastScroll = 0;
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll > lastScroll) {
+    // Scrolling down
+    navbar.style.transform = "translateY(-100%)";
+  } else {
+    // Scrolling up
+    navbar.style.transform = "translateY(0)";
+  }
+
+  lastScroll = currentScroll;
 });
